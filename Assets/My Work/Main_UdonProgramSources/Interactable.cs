6bb9ee6c665029c44b236f9480109261c4c425b5
JoycelynFoo <83127756+JoycelynFoo@users.noreply.github.com>
2023-12-11 14:38:42 +0900

    base

---
+++ Assets/My Work/Main_UdonProgramSources/Interactable.cs
@@ -0,0 +1,70 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Interactable : UdonSharpBehaviour
+{
+    private bool gotSandBags = false;
+    private int sandBags = 0; 
+    public TextMeshPro scoreText;
+
+    public GameObject glassWall;
+
+    private void Update()
+    {   
+        if (sandBags == 3)
+        {
+            
+            Destroy(glassWall);
+        }
+
+        if(gotSandBags == true)
+        {
+            gotSandBags = false;
+            sandBags += 1;
+            //scoreText.text = sandBags.ToString();
+            Debug.Log(sandBags);
+        }
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("sandBagTossBoard"))
+        {
+            gotSandBags = true;
+
+            //check if 3 bags are in, in yes, destroy glass and sand bag
+            //Destroy(gameObject);
+
+            //sandBags += 1 ;
+            //Debug.Log(sandBags);
+            //ChangeScore(1);
+
+            //if(sandBags >= minimumSandBags)
+            //{
+            //   Destroy(gameObject);
+            //}
+        }
+        //if (other.gameObject.name.Contains("SandBagDeadZone"))
+        //{
+        //    Destroy(gameObject);
+        //}
+
+        // if (sandBagHit == false)
+        //{
+        //    sandBag.SetActive(true);
+        //    sandBagHit = false;
+        //}
+        //else
+        //{
+        //    sandBag.SetActive(false);
+        //    sandBagHit = true;
+        //}
+    }
+}
---
+++ Assets/My Work/Main_UdonProgramSources/MIrror.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MIrror : UdonSharpBehaviour
+{
+    public GameObject mirror;
+    public bool mirrorOn;
+    
+
+    public override void Interact()
+    {
+        if(mirrorOn == false)
+        {
+            mirror.SetActive(true);
+            mirrorOn = true;
+        }
+        else
+        {
+            mirror.SetActive(false);
+            mirrorOn = false;
+        }
+       
+
+    }
+}
