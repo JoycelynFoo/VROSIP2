1fe53c39eddde91601241549469744560e69d642
JoycelynFoo <83127756+JoycelynFoo@users.noreply.github.com>
2024-01-04 20:48:23 +0900

    hmmmm

--- Assets/My Work/Main_UdonProgramSources/Basketball.cs
+++ Assets/My Work/Main_UdonProgramSources/Basketball.cs
@@ -3,19 +3,36 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using UnityEngine.UI;
+using TMPro;
 
 public class Basketball : UdonSharpBehaviour
 {
     private bool ballIn = false;
-
+    private int balls = 0;
+    public TextMeshPro scoreTextBall;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name.Contains("bball"))
         {
             ballIn = true;
-            Debug.Log("Bball In");
         }
     }
 
+
+    private void Update()
+    {
+        if (balls == 3)
+        {
+            Debug.Log("3 balls are in");
+        }
+        if (ballIn == true)
+        {
+            ballIn = false;
+            balls += 1;
+            scoreTextBall.text = balls.ToString();
+            Debug.Log(balls);
+        }
+    }
 }
